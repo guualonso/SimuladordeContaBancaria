5f63c2c211fe49a8117c2ed5b5c0884e81a1f37a
guualonso <gualonso14@gmail.com>
2025-02-13 18:45:52 -0300

    Criação da Conta Corrente

---
+++ src/ContaCorrente.java
@@ -0,0 +1,26 @@
+public class ContaCorrente extends ContaBancaria {
+    private double limiteChequeEspecial;
+
+    public ContaCorrente(int numeroDaConta, String titular, double saldoInicial, double limiteChequeEspecial) {
+        super(numeroDaConta, titular, saldoInicial);
+        this.limiteChequeEspecial = limiteChequeEspecial;
+    }
+
+    @Override
+    public void sacar(double valor) {
+        if (valor > 0 && (getSaldo() + limiteChequeEspecial >= valor)){
+            double novoSaldo = getSaldo() - valor;
+            if (novoSaldo < 0) {
+                limiteChequeEspecial += novoSaldo;
+                novoSaldo = 0;
+            }
+            System.out.println("Saque de R$ " + valor + "realizado com sucesso");
+        }
+        else {
+            System.out.println("Saldo insuficiente para saque.");
+        }
+    }
+    public void exibirLimite() {
+        System.out.println("Seu limite do cheque especial é de R$ " + limiteChequeEspecial);
+    }
+}
