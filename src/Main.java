2ab76bc4227fcb904bbd5b7223d47492532dc570
guualonso <gualonso14@gmail.com>
2025-02-13 19:14:34 -0300

    Criação do Main (Testes)

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,17 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        ContaPoupanca cp = new ContaPoupanca(54312, "Gustavo", 2000, 1.08);
+        cp.exibirSaldo();
+
+        // Simulando rendimento do CDI em 8 meses
+        cp.simularMeses(8);
+
+        //Atualizando a taxa CDI e simulando novamente
+
+        System.out.println("Atualizando taxa CDI...");
+        cp.setTaxaCDI(1.20);
+        cp.simularMeses(3);
     }
 }
